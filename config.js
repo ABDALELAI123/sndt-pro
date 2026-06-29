67aa293419761847a50dcbdacbe47e997cce5ce4
ABDALELAI123 <alnmrybdalalh22@gmail.com>
2026-06-29 18:55:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
-// config.js - إعدادات النظام الأساسية
+// config.js - إعدادات النظام الأساسية لسندات برو
+// v2.0.0 - 2026/06/29
+
 const firebaseConfig = {
   apiKey: "AIzaSyBvEU1Cmt1IX4h0xaCd6McaOMufvGqr2fY",
   authDomain: "sanadat-pro.firebaseapp.com",
@@ -11,12 +13,20 @@ const firebaseConfig = {
 
 const appConfig = {
   baseUrl: "https://abdalelai123.github.io/sndt-pro",
-  رمز_المشرف_الافتراضي: "001122334455",
-  إصدار_التطبيق: "1.0.0",
-  هاتف_الدعم: "0597087767"
+  superAdminCode: "001122334455",
+  superAdminPhone: "0597087767",
+  superAdminEmail: "001122334455-0597087767@sanadat.pro",
+  appVersion: "2.0.0",
+  appName: "سندات برو",
+  currency: "SAR",
+  currencyName: "ريال سعودي",
+  defaultLanguage: "ar",
+  dateFormat: "ar-SA"
 };
 
+// تصدير للـ ES Modules
+export { firebaseConfig, appConfig };
+
+// تصدير للـ window للملفات القديمة
 window.firebaseConfig = firebaseConfig;
 window.appConfig = appConfig;
-
-
